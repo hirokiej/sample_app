624f9ca7d77c13a7b8f3602eedd55267c8eb442b
hiro <your.email@example.com>
2023-04-04 17:08:28 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params [:session][:password])
+    if user && user.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
@@ -13,8 +14,9 @@ class SessionsController < ApplicationController
       render "new", status: :unprocessable_entity
     end
   end
+  
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
