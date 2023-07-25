116a40ddc606ad103cb00a6c76a49bf714cf3b3c
hiro <your.email@example.com>
2023-07-25 21:22:28 +0000

    Add passpord reset

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,7 +23,7 @@ module SessionsHelper
       end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
+        if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
         end
@@ -58,3 +58,4 @@ module SessionsHelper
     session[:forwarding_url] = request.original_url if request.get?  
   end
 end
+
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.comm"
+  default from: "from@example.com"
   layout "mailer"
 end
